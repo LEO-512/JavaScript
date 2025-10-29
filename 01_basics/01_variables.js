6636e710be9a403c4be2723f399bd45ab3002bf8
Lareb Malik <lareb1202@gmail.com>
2025-10-29 03:40:16 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "leo@gmail.com"
+var accountPassword = "1234" //don't use var 
+accountCity = "Islamabad" 
+let accountState;
+
+// accountId = 55 //const cannot be changed
+accountEmail = "le@gmail.com"
+accountPassword = "55"
+accountCity = "Karachi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version 
+
+//alert("Ho") using node.js
+
+let name = "Laraib" //string 
+let age = 21 //number(integer) 2 to power 53
+let isloggedin = false //boolean "yes" or "no" (true/false)
+//null = standalone value
+let state = 'null'
+//undefined
+let state2; 
+//symbol = unique (React)
+//object 
+
+console.log(typeof age);
+console.log(typeof isloggedin);
+console.log(typeof null); // null is a object 
+console.log(typeof undefined); //undefined 
